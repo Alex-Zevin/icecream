a75bf5ad745dcd01cfec79d96488d8c3476db5c2
Alex-Zevin <alex.ze@it-justice.com>
2022-10-11 12:39:38 +0300

    add footer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     return (
         <div className="App">
             <header>
-                <div className="header_all">
+                <div className="header_all container">
                     <img src={logo} alt="Logo"/>
                     <div className="header_nav">
                         <div className="header_nav_action">
@@ -21,6 +21,26 @@ function App() {
                     </div>
                 </div>
             </header>
+            <footer>
+                <div className="footer_all container">
+                    <img src={logo} alt="Logo"/>
+                    <div className="footer_nav">
+                        <ul className="footer_nav_list">
+                            <li className="footer_nav_list_item"><a href="#">Our Products</a></li>
+                            <li className="footer_nav_list_item"><a href="#">Privacy Terms</a></li>
+                            <li className="footer_nav_list_item"><a href="#">Twitter</a></li>
+                            <li className="footer_nav_list_item"><a href="#">Facebook</a></li>
+                            <li className="footer_nav_list_item"><a href="#">Email</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="copyright" >
+                    <div className="container">
+                    <span>© 2021 Justice-team. All rights reserved.</span>
+                    </div>
+                </div>
+            </footer>
+
         </div>
     );
 }
